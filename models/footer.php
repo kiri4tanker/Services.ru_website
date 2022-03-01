59b7e5b0410d2fbae0b6320cb50cde048d44e8d6
Kirill Terentiev <kiri4tankerofficial@gmail.com>
2022-03-01 18:31:14 +0200

    links + models

---
+++ models/footer.php
@@ -0,0 +1,35 @@
+   <!-- Start of Footer -->
+   <footer class="footer">
+      <div class="container">
+         <div class="footer__content">
+            <a class="logo" href="#">
+               <img src="assets/images/footer__logo.svg" alt="LOGO">
+            </a>
+            <nav class="nav">
+               <ul class="nav__menu">
+                  <li class="nav__item">
+                     <a class="nav__link" href="#">Главная</a>
+                  </li>
+                  <li class="nav__item">
+                     <a class="nav__link" href="#">Услуги</a>
+                  </li>
+                  <li class="nav__item">
+                     <a class="nav__link" href="#">О сервисе</a>
+                  </li>
+                  <li class="nav__item">
+                     <a class="nav__link" href="#">Контакты</a>
+                  </li>
+               </ul>
+            </nav>
+            <button class="btn"><ion-icon name="build-outline"></ion-icon></button>
+         </div>
+         <div class="footer__line"></div>
+         <div class="footer__bottom">
+               <p class="text-muted">© ООО "Услуги.ру" Все права защищены 2022</p>
+         </div>
+      </div>
+   </footer>
+   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html> 
---
+++ models/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="assets/css/main.css">
+   <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+   <title>Услуги.ру</title>
+</head>
+   <!-- Start of Header -->
+   <header class="header">
+      <div class="container">
+         <div class="header__content">
+            <a class="logo" href="index.php">
+               <img src="assets/images/header__logo.svg" alt="LOGO">
+            </a>
+            <nav class="nav">
+               <ul class="nav__menu">
+                  <li class="nav__item">
+                     <a class="nav__link" href="index.php">Главная</a>
+                  </li>
+                  <li class="nav__item">
+                     <a class="nav__link" href="services.php">Услуги</a>
+                  </li>
+                  <li class="nav__item">
+                     <a class="nav__link" href="about-us.php">О сервисе</a>
+                  </li>
+                  <li class="nav__item">
+                     <a class="nav__link" href="contacts.php">Контакты</a>
+                  </li>
+               </ul>
+            </nav>
+            <div class="inline">
+               <button class="btn"><ion-icon name="person-outline"></ion-icon>Войти</button>
+               <button class="btn"><ion-icon name="build-outline"></ion-icon></button>
+            </div>
+         </div> 
+      </div>
+   </header>
