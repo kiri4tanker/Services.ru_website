f7340513b85cfd55ca008bddc0a36cf185cd03c3
Kirill Terentiev <kiri4tankerofficial@gmail.com>
2022-03-03 21:51:44 +0200

    +404 + new bugs, ono krivoe, no xojet dishat

--- 404.php
+++ 404.php
@@ -1,2 +1,19 @@
 <?php require_once('include/header.php') ?>
+   <main class="main">
+      <section class="section section_primary">
+         <div class="container">
+            <div class="section__wrapper">
+               <div class="section__heading section__heading_grid">
+                  <h1 class="section__title">Ошибка 404</h1>
+                  <strong>Кажется, Вы потерялись!</strong>
+                  <p class="section__subtitle">Постарайтесь вернуться на рабочую страницу, а то Вас заждались дома, конечно, если есть кому ждать...</p>
+                  <button class="btn">На главную</button>
+               </div>
+               <div class="section__content">
+                  <img src="assets/images/section-img/404.png" alt="404-img" class="section__img">
+               </div>
+            </div>
+         </div>
+      </section>
+   </main>
 <?php require_once('include/footer.php') ?>
