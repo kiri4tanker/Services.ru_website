16ca02d4b8e84b572c45ef291ecba5369cf4523d
Kirill Terentiev <kiri4tankerofficial@gmail.com>
2022-03-06 12:45:06 +0200

    +contacts

--- contacts.php
+++ contacts.php
@@ -1,2 +1,27 @@
 <?php require_once('include/header.php') ?>
+   <!-- Start of Main -->
+   <main class="main">
+      <section class="section">
+         <div class="container">
+            <div class="section__wrapper">
+               <div class="contacts">
+                  <div class="section__heading">
+                     <h1 class="section__title">Контакты</h1>
+                     <div class="contact">
+                        <strong class="contacts__strong">Для партнёров</strong>
+                        <p class="contacts__text">Запускаем проекты, которые делают жизнь в городе лучше. Есть идеи? info@yslugi.com</p>
+                     </div>
+                     <div class="contact">
+                        <strong class="contacts__strong">Для СМИ и PR-служб</strong>
+                        <p class="contacts__text">Запускаем проекты, которые делают жизнь в городе лучше. Есть идеи? info@yslugi.com</p>
+                     </div>
+                  </div>
+                  <div class="section__content">
+                     <img src="assets/images/section-img/contact.png" alt="contant-img" class="section__img">
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+   </main>
 <?php require_once('include/footer.php') ?>
