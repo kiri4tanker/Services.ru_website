dab164913fb2e8d847fc2849f37c7812da7e5bf5
Kirill Terentiev <kiri4tankerofficial@gmail.com>
2022-03-08 22:25:25 +0200

    +krasivoex2

--- 404.php
+++ 404.php
@@ -4,14 +4,16 @@
       <section class="section section_primary">
          <div class="container">
             <div class="section__wrapper">
-               <div class="section__heading section__heading_grid">
-                  <h1 class="section__title">Ошибка 404</h1>
-                  <strong>Кажется, Вы потерялись!</strong>
-                  <p class="section__subtitle">Постарайтесь вернуться на рабочую страницу, а то Вас заждались дома, конечно, если есть кому ждать...</p>
-                  <a href="index.php" class="btn">На главную</a>
-               </div>
                <div class="section__content">
-                  <img src="assets/images/section-img/404.png" alt="404-img" class="section__img">
+                  <div class="error">
+                     <div class="error__content">
+                        <h1 class="section__title">Ошибка 404</h1>
+                        <strong>Кажется, Вы потерялись!</strong>
+                        <p class="section__subtitle">Постарайтесь вернуться на рабочую страницу, а то Вас заждались дома, конечно, если есть кому ждать...</p>
+                        <a href="index.php" class="btn">На главную</a>
+                     </div>
+                     <img src="assets/images/section-img/404.png" alt="404-img" class="error__img">
+                  </div>
                </div>
             </div>
          </div>
