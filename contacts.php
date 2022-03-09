22d8dc6459b89bf829a9dcb0f0234af1b7b886f3
Kirill Terentiev <kiri4tankerofficial@gmail.com>
2022-03-09 19:11:46 +0200

    +fix email links

--- contacts.php
+++ contacts.php
@@ -10,11 +10,11 @@
                      <h1 class="section__title">Контакты</h1>
                      <div class="contact">
                         <strong class="contacts__strong">Для партнёров</strong>
-                        <p class="contacts__text">Запускаем проекты, которые делают жизнь в городе лучше. Есть идеи? info@yslugi.com</p>
+                        <p class="contacts__text">Запускаем проекты, которые делают жизнь в городе лучше. Есть идеи? <a href="mailto:info@yslugi.com">info@yslugi.com</a></p>
                      </div>
                      <div class="contact">
                         <strong class="contacts__strong">Для СМИ и PR-служб</strong>
-                        <p class="contacts__text">Запускаем проекты, которые делают жизнь в городе лучше. Есть идеи? info@yslugi.com</p>
+                        <p class="contacts__text">Запускаем проекты, которые делают жизнь в городе лучше. Есть идеи? <a href="mailto:info@yslugi.com">info@yslugi.com</a></p>
                      </div>
                   </div>
                   <div class="section__content">
